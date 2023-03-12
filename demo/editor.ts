e12f668f15da3ce3548ddba3e56843f05d88cd6e
Tristan F <LeoDog896@hotmail.com>
2023-03-11 19:25:41 -0500

    docs: establish tree output

--- demo/editor.ts
+++ demo/editor.ts
@@ -1,7 +1,24 @@
 import { EditorView, basicSetup } from "codemirror"
-import { SNBT } from "../dist/index.js"
+import { SNBT, SNBTLanguage } from "../dist/index.js"
+
+// SNBTLanguage.parser.parse(
+
+const editor = document.querySelector<HTMLDivElement>("div#editor")!
+const output = document.querySelector<HTMLDivElement>("div#output")!
 
 new EditorView({
-  extensions: [basicSetup, SNBT()],
-  parent: document.body
+  extensions: [
+    basicSetup,
+    SNBT(),
+    EditorView.updateListener.of((e) => {
+      try {
+        const tree = SNBTLanguage.parser.parse(e.state.doc.toString());
+        output.innerText = JSON.stringify(tree.children)
+      } catch (e) {
+        output.innerText = e.message;
+        console.error(e);
+      }
+    })
+  ],
+  parent: editor
 })
